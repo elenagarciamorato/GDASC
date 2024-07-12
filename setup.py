02ee9a9a9506bb98d17207889c061cdfc86af811
egarciap <elena.garciamorato@urjc.es>
2024-07-12 10:50:39 +0200

    requirements.txt and setup.py files included
    TODO:
    README.md
    include some experiments results
    
    Signed-off-by: egarciap <elena.garciamorato@urjc.es>

---
+++ setup.py
@@ -0,0 +1,15 @@
+from setuptools import setup
+
+setup(
+    name='GDASC',
+    version='',
+    packages=['GDASC', 'GDASC.clustering_algorithms', 'benchmarks', 'benchmarks.plotting', 'benchmarks.algorithms',
+              'benchmarks.algorithms.LSH', 'benchmarks.algorithms.Exact', 'benchmarks.algorithms.FLANN',
+              'benchmarks.algorithms.FLANN.Centroids', 'benchmarks.algorithms.GDASC',
+              'benchmarks.algorithms.LSH_Forest', 'benchmarks.algorithms.Pynndescent'],
+    url='',
+    license='',
+    author='elenagarciamorato',
+    author_email='',
+    description=''
+)
