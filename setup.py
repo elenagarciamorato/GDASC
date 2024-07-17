1dc49d1f9ab5f824a3342664225975853bda34e4
egarciap <elena.garciamorato@urjc.es>
2024-07-17 17:34:48 +0200

    requirements.txt and setup.py updated
    TODO:
    Include README.md
    Include License
    Improve experiment launching
    
    Signed-off-by: egarciap <elena.garciamorato@urjc.es>

--- setup.py
+++ setup.py
@@ -1,15 +1,23 @@
-from setuptools import setup
+try:
+    from setuptools import setup, find_packages
+except ImportError:
+    from distutils.core import setup, find_packages
+
+requires = []
+
+
+with open('requirements.txt') as fp:
+    requires = fp.read()
 
 setup(
     name='GDASC',
-    version='',
-    packages=['GDASC', 'GDASC.clustering_algorithms', 'benchmarks', 'benchmarks.plotting', 'benchmarks.algorithms',
-              'benchmarks.algorithms.LSH', 'benchmarks.algorithms.Exact', 'benchmarks.algorithms.FLANN',
-              'benchmarks.algorithms.FLANN.Centroids', 'benchmarks.algorithms.GDASC',
-              'benchmarks.algorithms.LSH_Forest', 'benchmarks.algorithms.Pynndescent'],
-    url='',
+    version='0.0.1',
+    url='https://github.com/elenagarciamorato/GDASC',
     license='',
-    author='elenagarciamorato',
-    author_email='',
-    description=''
+    author='Elena Garcia-Morato, Maria Jesus Algar, Cesar Alfaro, Felipe Ortega, Javier Gomez and Javier M. Moguerza',
+    author_email='elena.garciamorato@urjc.es, mariajesus.algar@urjc.es, cesar.alfaro@urjc.es, felipe.ortega@urjc.es, javier.gomez@urjc.es and javier.moguerza@urjc.es',
+    description='',
+    packages=find_packages(),
+    platforms='any',
+    install_requires=requires
 )
